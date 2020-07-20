44e5065f96decc9e9258126923a1724f311e869a
kadircekim07 <33603691+kadircekim07@users.noreply.github.com>
2020-07-20 18:24:11 +0300

    fix

--- index.php
+++ index.php
@@ -1,19 +1,33 @@
 <?php 
-$id = $_GET['id']; //the youtube video ID
-$format = $_GET['fmt']; //the MIME type of the video. e.g. video/mp4, video/webm, etc.
-parse_str(file_get_contents("http://youtube.com/get_video_info?video_id=".$id),$info); //decode the data
-$streams = $info['url_encoded_fmt_stream_map']; //the video's location info
+
+$id = 'RllJtOw0USI'; // just in case
+
+if (isset($_GET["id"]))
+	$id = $_GET["id"];
+
+parse_str(file_get_contents('http://www.youtube.com/get_video_info?video_id='.$id), $video_data);
+
+$streams = $video_data['url_encoded_fmt_stream_map'];
 $streams = explode(',',$streams);
-foreach($streams as $stream){
-    parse_str($stream,$data); //decode the stream
-    if(stripos($data['type'],$format) !== false){ //We've found the right stream with the correct format
-        $video = fopen($data['url'].'&signature='.$data['sig'],'r'); //the video
-        $file = fopen('video.'.str_replace($format,'video/',''),'w');
-        stream_copy_to_stream($video,$file); //copy it to the file
-        fclose($video);
-        fclose($file);
-        echo 'Download finished! Check the file.';
-        break;
-    }
+$counter = 1;
+
+foreach ($streams as $streamdata) {
+	printf("Stream %d:<br/>----------------<br/><br/>", $counter);
+	
+	parse_str($streamdata,$streamdata);
+
+	foreach ($streamdata as $key => $value) {
+		if ($key == "url") {
+			$value = urldecode($value);
+			printf("<strong>%s:</strong> <a href='%s'>video link</a><br/>", $key, $value);
+		} else {
+			printf("<strong>%s:</strong> %s<br/>", $key, $value);
+		}
+	}
+
+	$counter = $counter+1;
+	printf("<br/><br/>");
 }
+
+// start server and go to http://url/?id=video-id
 ?>
