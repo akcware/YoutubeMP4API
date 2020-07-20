72b246dd15c656908b93355a64023ee94bc78445
kadircekim07 <33603691+kadircekim07@users.noreply.github.com>
2020-07-20 18:14:25 +0300

    fix

--- index.php
+++ index.php
@@ -1,112 +1,19 @@
-<?php
-header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json');
-
-
-function get_error($ErrorExaction){
-    $myObj = new stdClass();
-    $myObj->error = true;
-    $myObj->msg = $ErrorExaction;
-
-    $myObj->madeBy = "A.El-zahaby";
-    $myObj->instagram = "egy.js";
-    $myJSON = json_encode($myObj,JSON_PRETTY_PRINT);
-    echo $myJSON;
-    exit;
-}
-
-function get_log($dump){
-    if (isset($_GET['log'])) var_dump($dump).'\n\n\n';
-}
-if(isset($_GET['url']) && $_GET['url'] != ""){
-    
-    parse_str( parse_url( $_GET['url'], PHP_URL_QUERY ), $vars );
-
-
-    $id=$vars['v'];
-    $dt=file_get_contents("https://www.youtube.com/get_video_info?video_id=$id&el=embedded&ps=default&eurl=&gl=US&hl=en");
-    if (strpos($dt, 'status=fail') !== false) {
-
-        $x=explode("&",$dt);
-        $t=array(); $g=array(); $h=array();
-
-        foreach($x as $r){
-            $c=explode("=",$r);
-            $n=$c[0]; $v=$c[1];
-            $y=urldecode($v);
-            $t[$n]=$v;
-        }
-
-        $x=explode("&",$dt);
-        foreach($x as $r){
-            $c=explode("=",$r);
-            $n=$c[0]; $v=$c[1];
-            $h[$n]=urldecode($v);
-        }
-        $g[]=$h;
-        $g[0]['error'] = true;
-        $g[0]['instagram'] = "egy.js";
-        $g[0]['apiMadeBy'] = 'El-zahaby';
-        echo json_encode($g,JSON_PRETTY_PRINT);
-
-    }else{
-
-        $x=explode("&",$dt);
-        $t=array(); $g=array(); $h=array();
-
-        foreach($x as $r){
-            $c=explode("=",$r);
-            $n=$c[0]; $v=$c[1];
-            $y=urldecode($v);
-            $t[$n]=$v;
-        }
-        $streams = explode(',',urldecode($t['url_encoded_fmt_stream_map']));
-//        if(empty($streams[0])){ get_error('ops! this video has something wrong! :( '); }
-        if(empty($streams[0])){
-            get_log($streams);
-        }
-        foreach($streams as $dt){
-            $x=explode("&",$dt);
-            foreach($x as $r){
-                $c=explode("=",$r);
-                if ($c[0]  == 'itag'){ // reference:  https://superuser.com/q/1386658
-                    switch ($c[1]){
-                        case '18':
-                            $h['mimeType'] = "mp4";
-                            $h['width'] = "640";
-                            $h['height'] = "360";
-                            $h['qualityLabel'] = '360p';
-                            break;
-                        case '22':
-                            $h['mimeType'] = "mp4";
-                            $h['width'] = "1280";
-                            $h['height'] = "720";
-                            $h['qualityLabel'] = '720p';
-                            break;
-                        case '43':
-                            $h['mimeType'] = "webm";
-                            $h['width'] = "640";
-                            $h['height'] = "360";
-                            $h['qualityLabel'] = '360p';
-                            break;
-                        default:
-                            $h['mimeType'] = null;
-                            $h['width'] = null;
-                            $h['height'] = null;
-                            $h['qualityLabel'] = '';
-                    }
-                }
-                $n=$c[0]; /* => */ $v=$c[1];
-                $h[$n]=urldecode($v);
-
-            }
-            $g[]=$h;
-        }
-        echo json_encode($g,JSON_PRETTY_PRINT);
-        // var_dump( $g[1]["quality"],true);
+<?php 
+$id = $_GET['id']; //the youtube video ID
+$format = $_GET['fmt']; //the MIME type of the video. e.g. video/mp4, video/webm, etc.
+parse_str(file_get_contents("http://youtube.com/get_video_info?video_id=".$id),$info); //decode the data
+$streams = $info['url_encoded_fmt_stream_map']; //the video's location info
+$streams = explode(',',$streams);
+foreach($streams as $stream){
+    parse_str($stream,$data); //decode the stream
+    if(stripos($data['type'],$format) !== false){ //We've found the right stream with the correct format
+        $video = fopen($data['url'].'&signature='.$data['sig'],'r'); //the video
+        $file = fopen('video.'.str_replace($format,'video/',''),'w');
+        stream_copy_to_stream($video,$file); //copy it to the file
+        fclose($video);
+        fclose($file);
+        echo 'Download finished! Check the file.';
+        break;
     }
-}else{
-
-     get_error("Ops, there is no youtube link!");
-
 }
+?>
